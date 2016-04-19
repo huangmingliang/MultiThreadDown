d0fd1e9db2c937c3b4152ca42e72655684e7b5b5
huangmingliang <1012432649@qq.com>
2016-04-19 10:15:20 +0800

    修改字符编码

--- app/src/main/java/com/example/multithreaddown/DownUtil.java
+++ app/src/main/java/com/example/multithreaddown/DownUtil.java
@@ -1,9 +1,4 @@
-/**
- *
- */
 package com.example.multithreaddown;
-
-import android.util.Log;
 
 import java.io.InputStream;
 import java.io.RandomAccessFile;
@@ -12,152 +7,138 @@ import java.net.URL;
 
 /**
  * Description:
- * <br/>???: <a href="http://www.crazyit.org">???Java????</a>
+ * <br/>网站: <a href="http://www.crazyit.org">疯狂Java联盟</a>
  * <br/>Copyright (C), 2001-2014, Yeeku.H.Lee
  * <br/>This program is protected by copyright laws.
  * <br/>Program Name:
  * <br/>Date:
- * @author  Yeeku.H.Lee kongyeeku@163.com
- * @version  1.0
+ *
+ * @author Yeeku.H.Lee kongyeeku@163.com
+ * @version 1.0
  */
-public class DownUtil
-{
-	private String TAG=getClass().getSimpleName();
-	// ????????????????
-	private String path;
-	// ?????????????????????
-	private String targetFile;
-	// ???????????????????????
-	private int threadNum;
-	// ???????????????
-	private DownThread[] threads;
-	// ??????????????????
-	private int fileSize;
+public class DownUtil {
+    // 定义下载资源的路径
+    private String path;
+    // 指定所下载的文件的保存位置
+    private String targetFile;
+    // 定义需要使用多少线程下载资源
+    private int threadNum;
+    // 定义下载的线程对象
+    private DownThread[] threads;
+    // 定义下载的文件的总大小
+    private int fileSize;
 
-	public DownUtil(String path, String targetFile, int threadNum)
-	{
-		this.path = path;
-		this.threadNum = threadNum;
-		// ?????threads????
-		threads = new DownThread[threadNum];
-		this.targetFile = targetFile;
-	}
+    public DownUtil(String path, String targetFile, int threadNum) {
+        this.path = path;
+        this.threadNum = threadNum;
+        // 初始化threads数组
+        threads = new DownThread[threadNum];
+        this.targetFile = targetFile;
+    }
 
-	public void download() throws Exception
-	{
-		Log.e(TAG,"�ļ���ַ��"+path);
-		URL url = new URL(path);
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setConnectTimeout(5 * 1000);
-		conn.setRequestMethod("GET");
-		conn.setRequestProperty(
-			"Accept",
-			"image/gif, image/jpeg, image/pjpeg, image/pjpeg, "
-			+ "application/x-shockwave-flash, application/xaml+xml, "
-			+ "application/vnd.ms-xpsdocument, application/x-ms-xbap, "
-			+ "application/x-ms-application, application/vnd.ms-excel, "
-			+ "application/vnd.ms-powerpoint, application/msword, */*");
-		conn.setRequestProperty("Accept-Language", "zh-CN");
-		conn.setRequestProperty("Charset", "UTF-8");
-		conn.setRequestProperty("Connection", "Keep-Alive");
-		// ?????????
-		fileSize = conn.getContentLength();
-		conn.disconnect();
-		int currentPartSize = fileSize / threadNum + 1;
-		RandomAccessFile file = new RandomAccessFile(targetFile, "rw");
-		// ??????????????
-		file.setLength(fileSize);
-		file.close();
-		for (int i = 0; i < threadNum; i++)
-		{
-			// ?????????????????????
-			int startPos = i * currentPartSize;
-			// ????????????RandomAccessFile????????
-			RandomAccessFile currentPart = new RandomAccessFile(targetFile,
-				"rw");
-			// ????????????????
-			currentPart.seek(startPos);
-			// ???????????
-			threads[i] = new DownThread(startPos, currentPartSize,
-				currentPart);
-			// ???????????
-			threads[i].start();
-		}
-	}
+    public void download() throws Exception {
+        URL url = new URL(path);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setConnectTimeout(5 * 1000);
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty(
+                "Accept",
+                "image/gif, image/jpeg, image/pjpeg, image/pjpeg, "
+                        + "application/x-shockwave-flash, application/xaml+xml, "
+                        + "application/vnd.ms-xpsdocument, application/x-ms-xbap, "
+                        + "application/x-ms-application, application/vnd.ms-excel, "
+                        + "application/vnd.ms-powerpoint, application/msword, */*");
+        conn.setRequestProperty("Accept-Language", "zh-CN");
+        conn.setRequestProperty("Charset", "UTF-8");
+        conn.setRequestProperty("Connection", "Keep-Alive");
+        // 得到文件大小
+        fileSize = conn.getContentLength();
+        conn.disconnect();
+        int currentPartSize = fileSize / threadNum + 1;
+        RandomAccessFile file = new RandomAccessFile(targetFile, "rw");
+        // 设置本地文件的大小
+        file.setLength(fileSize);
+        file.close();
+        for (int i = 0; i < threadNum; i++) {
+            // 计算每条线程的下载的开始位置
+            int startPos = i * currentPartSize;
+            // 每个线程使用一个RandomAccessFile进行下载
+            RandomAccessFile currentPart = new RandomAccessFile(targetFile,
+                    "rw");
+            // 定位该线程的下载位置
+            currentPart.seek(startPos);
+            // 创建下载线程
+            threads[i] = new DownThread(startPos, currentPartSize,
+                    currentPart);
+            // 启动下载线程
+            threads[i].start();
+        }
+    }
 
-	// ??????????????
-	public double getCompleteRate()
-	{
-		// ?????????????????????
-		int sumSize = 0;
-		for (int i = 0; i < threadNum; i++)
-		{
-			if (threads[i]!=null) {
-				sumSize += threads[i].length;
-			}
-		}
-		// ??????????????
-		return sumSize * 1.0 / fileSize;
-	}
+    // 获取下载的完成百分比
+    public double getCompleteRate() {
+        // 统计多条线程已经下载的总大小
+        int sumSize = 0;
+        for (int i = 0; i < threadNum; i++) {
+            if (threads[i] != null) {
+                sumSize += threads[i].length;
+            }
+        }
+        // 返回已经完成的百分比
+        return sumSize * 1.0 / fileSize;
+    }
 
-	private class DownThread extends Thread
-	{
-		// ??????????????
-		private int startPos;
-		// ??????????????????????
-		private int currentPartSize;
-		// ???????????????????
-		private RandomAccessFile currentPart;
-		// ????????????????????????
-		public int length;
+    private class DownThread extends Thread {
+        // 当前线程的下载位置
+        private int startPos;
+        // 定义当前线程负责下载的文件大小
+        private int currentPartSize;
+        // 当前线程需要下载的文件块
+        private RandomAccessFile currentPart;
+        // 定义已经该线程已下载的字节数
+        public int length;
 
-		public DownThread(int startPos, int currentPartSize,
-			RandomAccessFile currentPart)
-		{
-			this.startPos = startPos;
-			this.currentPartSize = currentPartSize;
-			this.currentPart = currentPart;
-		}
+        public DownThread(int startPos, int currentPartSize,
+                          RandomAccessFile currentPart) {
+            this.startPos = startPos;
+            this.currentPartSize = currentPartSize;
+            this.currentPart = currentPart;
+        }
 
-		@Override
-		public void run()
-		{
-			try
-			{
-				URL url = new URL(path);
-				HttpURLConnection conn = (HttpURLConnection)url
-					.openConnection();
-				conn.setConnectTimeout(5 * 1000);
-				conn.setRequestMethod("GET");
-				conn.setRequestProperty(
-					"Accept",
-					"image/gif, image/jpeg, image/pjpeg, image/pjpeg, "
-					+ "application/x-shockwave-flash, application/xaml+xml, "
-					+ "application/vnd.ms-xpsdocument, application/x-ms-xbap, "
-					+ "application/x-ms-application, application/vnd.ms-excel, "
-					+ "application/vnd.ms-powerpoint, application/msword, */*");
-				conn.setRequestProperty("Accept-Language", "zh-CN");
-				conn.setRequestProperty("Charset", "UTF-8");
-				InputStream inStream = conn.getInputStream();
-				// ???startPos?????????????????????????????????????
-				inStream.skip(this.startPos);
-				byte[] buffer = new byte[1024];
-				int hasRead = 0;
-				// ?????????????????????
-				while (length < currentPartSize
-					&& (hasRead = inStream.read(buffer)) > 0)
-				{
-					currentPart.write(buffer, 0, hasRead);
-					// ????????????????
-					length += hasRead;
-				}
-				currentPart.close();
-				inStream.close();
-			}
-			catch (Exception e)
-			{
-				e.printStackTrace();
-			}
-		}
-	}
+        @Override
+        public void run() {
+            try {
+                URL url = new URL(path);
+                HttpURLConnection conn = (HttpURLConnection) url
+                        .openConnection();
+                conn.setConnectTimeout(5 * 1000);
+                conn.setRequestMethod("GET");
+                conn.setRequestProperty(
+                        "Accept",
+                        "image/gif, image/jpeg, image/pjpeg, image/pjpeg, "
+                                + "application/x-shockwave-flash, application/xaml+xml, "
+                                + "application/vnd.ms-xpsdocument, application/x-ms-xbap, "
+                                + "application/x-ms-application, application/vnd.ms-excel, "
+                                + "application/vnd.ms-powerpoint, application/msword, */*");
+                conn.setRequestProperty("Accept-Language", "zh-CN");
+                conn.setRequestProperty("Charset", "UTF-8");
+                InputStream inStream = conn.getInputStream();
+                // 跳过startPos个字节，表明该线程只下载自己负责哪部分文件。
+                inStream.skip(this.startPos);
+                byte[] buffer = new byte[1024];
+                int hasRead = 0;
+                // 读取网络数据，并写入本地文件
+                while (length < currentPartSize
+                        && (hasRead = inStream.read(buffer)) > 0) {
+                    currentPart.write(buffer, 0, hasRead);
+                    // 累计该线程下载的总大小
+                    length += hasRead;
+                }
+                currentPart.close();
+                inStream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/multithreaddown/MultiThreadDown.java
+++ app/src/main/java/com/example/multithreaddown/MultiThreadDown.java
@@ -1,5 +1,4 @@
 package com.example.multithreaddown;
-
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -12,6 +11,8 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+
+import com.example.multithreaddown.DownUtil;
 
 /**
  * Description:
@@ -37,12 +38,12 @@ public class MultiThreadDown extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		// ��ȡ��������е��������ؼ�
+		// 获取程序界面中的三个界面控件
 		url = (EditText) findViewById(R.id.url);
 		target = (EditText) findViewById(R.id.target);
 		downBn = (Button) findViewById(R.id.down);
 		bar = (ProgressBar) findViewById(R.id.bar);
-		// ����һ��Handler����
+		// 创建一个Handler对象
 		final Handler handler = new Handler()
 		{
 			@Override
@@ -59,9 +60,9 @@ public class MultiThreadDown extends Activity
 			@Override
 			public void onClick(View v)
 			{
-				// ��ʼ��DownUtil�������һ������ָ���߳���
+				// 初始化DownUtil对象（最后一个参数指定线程数）
 				downUtil = new DownUtil(url.getText().toString(),
-					target.getText().toString(), 6);
+						target.getText().toString(), 6);
 				new Thread()
 				{
 					@Override
@@ -69,26 +70,26 @@ public class MultiThreadDown extends Activity
 					{
 						try
 						{
-							// ��ʼ����
+							// 开始下载
 							downUtil.download();
 						}
 						catch (Exception e)
 						{
 							e.printStackTrace();
 						}
-						// ����ÿ����Ȼ�ȡһ��ϵͳ����ɽ��
+						// 定义每秒调度获取一次系统的完成进度
 						final Timer timer = new Timer();
 						timer.schedule(new TimerTask()
 						{
 							@Override
 							public void run()
 							{
-								// ��ȡ�����������ɱ���
+								// 获取下载任务的完成比率
 								double completeRate = downUtil.getCompleteRate();
 								mDownStatus = (int) (completeRate * 100);
-								// ������Ϣ֪ͨ������½����
+								// 发送消息通知界面更新进度条
 								handler.sendEmptyMessage(0x123);
-								// ������ȫ��ȡ���������
+								// 下载完全后取消任务调度
 								if (mDownStatus >= 100)
 								{
 									timer.cancel();
